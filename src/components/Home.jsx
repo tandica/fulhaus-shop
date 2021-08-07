91f1ae246e83d8da1aaf27547b24125901f13489
tandica <williams.tandica@gmail.com>
2021-08-06 21:17:47 -0400

    update home component with bg photo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,14 @@ import "../styles/Home.scss";
 
 export default function Home() {
   return (
-    <div className="home-container">
-      <div className="home-title">
-        <h1>Patio Furniture</h1>
+    <div>
+      <div className="home-container"></div>
+      {/* <div className="home-title">
+        <div>Patio Furniture</div>
         <button type="button" className="home-button">
           SHOP
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
