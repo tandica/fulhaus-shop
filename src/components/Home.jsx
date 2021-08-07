8361d7e7ac17936c8caad066e61976fbd39883bb
tandica <williams.tandica@gmail.com>
2021-08-06 21:27:54 -0400

    add title for home component with styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,12 @@ export default function Home() {
   return (
     <div>
       <div className="home-container"></div>
-      {/* <div className="home-title">
-        <div>Patio Furniture</div>
+      <div className="home-title">
+        <h1>Patio Furniture</h1>
         <button type="button" className="home-button">
           SHOP
         </button>
-      </div> */}
+      </div>
     </div>
   );
 }
