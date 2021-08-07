224515d0902167df252039476a6abc000421ee3e
tandica <williams.tandica@gmail.com>
2021-08-06 22:46:12 -0400

    move api functions to home.jsx and create onclick to call it

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import "../styles/Home.scss";
 
 export default function Home() {
+  const [product, setProduct] = useState([]);
+
+  async function getProducts(e) {
+    e.preventDefault();
+    const data = await fetch(
+      `https://main-api.fulhaus.com/fulhaus-tech-test/get-products`
+    )
+      .then((res) => res.json())
+      .then(
+        (data) => data
+        //const productInfo = [...new Set(data.map((product) => product))];
+        // console.log("INFO $$$", productInfo);
+        // console.log("product data*****", data);
+        // console.log("product data-----", data[0].color);
+        //setProduct(productInfo);
+      );
+    setProduct(data);
+  }
+  console.log("----", product);
   return (
     <div>
       <div className="home-container"></div>
       <div className="home-title">
         <h1>Patio Furniture</h1>
-        <button type="button" className="home-button">
+        <button
+          type="submit"
+          className="home-button"
+          onClick={(e) => getProducts(e)}
+        >
           SHOP
         </button>
       </div>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,20 +2,31 @@ import { useState } from "react";
 import "../styles/Products.scss";
 
 export default function Products() {
-  const [product, setProduct] = useState();
+  const [product, setProduct] = useState([]);
 
-  function getProducts() {
+  async function getProducts() {
     //e.preventDefault();
-    const data = fetch(
+    const data = await fetch(
       `https://main-api.fulhaus.com/fulhaus-tech-test/get-products`
     )
       .then((res) => res.json())
-      .then((data) => {
-        console.log("product data*****", data);
-      });
+      .then(
+        (data) => data
+        //const productInfo = [...new Set(data.map((product) => product))];
+        // console.log("INFO $$$", productInfo);
+        // console.log("product data*****", data);
+        // console.log("product data-----", data[0].color);
+        //setProduct(productInfo);
+      );
+    setProduct(data);
   }
+  console.log("----", product);
+  //getProducts();
+  //console.log(getProducts());
 
-  console.log(getProducts());
+  // function productDisplay() {
+  //   const productToDisplay =
+  // }
 
   return (
     <div>
